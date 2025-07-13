1a44d8aad1a5cdea112df7d50751e84c421a5bda
templar-ajay <templar.ajay@gmail.com>
2025-07-13 17:15:34 +0530

    init

---
+++ config/site.ts
@@ -0,0 +1,26 @@
+export type SiteConfig = typeof siteConfig;
+
+export const siteConfig = {
+  name: "Chessboard Image to Fen",
+  description:
+    "Convert chessboard images to fen code using machine learning tools - MobileNet v3 and yolo. Remake by Ajay Develops",
+  navItems: [
+    {
+      label: "Train Piece Recognition Model",
+      href: "/train-piece-recognition-model",
+    },
+  ],
+  navMenuItems: [
+    {
+      label: "Train Piece Recognition Model",
+      href: "/train-piece-recognition-model",
+    },
+  ],
+  links: {
+    github: "https://github.com/ajay-develops",
+    twitter: "https://x.com/ajay_develops",
+    docs: "https://heroui.com",
+    discord: "https://discord.gg/BgrPcPc89M",
+    sponsor: "https://www.patreon.com/c/AjayDevelops",
+  },
+};
